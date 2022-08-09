b66df6309727c06bbe6c1e54a946605713c1a883
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-08-09 15:16:44 -0300

    style of the buttons

--- src/Category/index.jsx
+++ src/Category/index.jsx
@@ -8,22 +8,22 @@ import React from 'react';
 function Category() {
   return (
     <List>
-      {/* <NavLink to={'/cuisine/japanese'}>
-        <GiChopsticks />
-        <h4>Japanese</h4>
-      </NavLink> */}
-      <NavLink to={'/cuisine/italian'}>
-        <FaPizzaSlice />
-        <h4>Italian</h4>
-      </NavLink>
-      <NavLink to={'/cuisine/american'}>
+      <SLink to={'/cuisine/american'}>
         <FaHamburger />
         <h4>American</h4>
-      </NavLink>
-      <NavLink to={'/cuisine/thai'}>
+      </SLink>
+      <SLink to={'/cuisine/italian'}>
+        <FaPizzaSlice />
+        <h4>Italian</h4>
+      </SLink>
+      {/* <SLink to={'/cuisine/japanese'}>
+        <GiChopsticks />
+        <h4>Japanese</h4>
+      </SLink> */}
+      <SLink to={'/cuisine/thai'}>
         <GiNoodles />
         <h4>Thai</h4>
-      </NavLink>
+      </SLink>
     </List>
   );
 }
@@ -33,4 +33,33 @@ const List = styled.div`
   display: flex;
   justify-content: center;
   margin: 2rem 0;
+`;
+
+const SLink = styled(NavLink)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  margin-right: 2rem;
+  background: linear-gradient(35deg, #494949, #313131);
+  width: 6rem;
+  height: 6rem;
+  cursor: pointer;
+  transform: scale(0.8);
+  &:hover {
+    transform: scale(0.9);
+    transition: all 0.3s;
+  }
+  h4 {
+    color: white;
+    font-size: 0.8rem;
+  }
+  svg {
+    color: white;
+    font-size: 1.5rem;
+  }
+  &.active {
+    background: linear-gradient(to right, #f27121, #e94057);
+  }
 `;
