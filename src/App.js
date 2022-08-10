2d07c3053fa390ffbdad691ee437fc94c8aa7ba6
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-08-10 11:21:38 -0300

    Search bar styled, and particaly functional

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Category from './components/Category';
 import Pages from './pages/Pages';
 import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './components/GlobalStyle';
+import Search from './components/Search';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <GlobalStyle />
+        <Search />
         <Category />
         <Pages />
       </BrowserRouter>
--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -3,7 +3,48 @@ import styled from 'styled-components';
 import { FaSearch } from 'react-icons/fa';
 
 function Search() {
-  return <div>Search</div>;
+  const formSearch = document.getElementById('search');
+
+  const [input, setInput] = useState('');
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <FromStyle onSubmit={submitHandler}>
+      <label htmlFor='search' className='sr-only'>
+        Search for certain recipes
+      </label>
+      <FaSearch onClick={() => formSearch.focus()} />
+      <input
+        type='text'
+        id='search'
+        name='search'
+        onChange={(e) => setInput(e.target.value)}
+      />
+    </FromStyle>
+  );
 }
+
+const FromStyle = styled.form`
+  position: relative;
+  text-align: center;
+  cursor: text;
+  input {
+    width: 70%;
+    border: none;
+    background: linear-gradient(35deg, #494949, #313131);
+    font-size: 1rem;
+    padding: 0.8rem 3rem;
+    border-radius: 1rem;
+    color: white;
+  }
+  svg {
+    position: absolute;
+    top: 50%;
+    transform: translate(100%, -50%);
+    color: white;
+  }
+`;
 
 export default Search;
